d16c66ed5626eef7eaafc14e8ba34d61837edb3d
Gabriel Valiente <105058637+gsvaliente@users.noreply.github.com>
2025-03-18 10:29:02 +0100

    Add SideSheet component and Sheet UI elements

---
+++ components/shared/header/SideSheet.tsx
@@ -0,0 +1,44 @@
+import Link from 'next/link'
+
+import { Button } from '@/components/ui/button'
+import { EllipsisVertical, ShoppingCart, UserIcon } from 'lucide-react'
+import {
+  Sheet,
+  SheetContent,
+  SheetDescription,
+  SheetTitle,
+  SheetTrigger,
+} from '@/components/ui/sheet'
+
+import { ModeToggle } from '@/components/shared/header/Mode-toggle'
+
+export function SideSheet() {
+  return (
+    <nav className='md:hidden'>
+      <Sheet>
+        <SheetTrigger className='align-middle'>
+          <EllipsisVertical />
+        </SheetTrigger>
+        <SheetContent className='flex flex-col items-start'>
+          <SheetTitle>Menu</SheetTitle>
+          <ModeToggle />
+          <Button
+            asChild
+            variant='ghost'>
+            <Link href='/cart'>
+              <ShoppingCart /> Cart
+            </Link>
+          </Button>
+          <Button
+            asChild
+            variant='default'>
+            <Link href='/sign-in'>
+              <UserIcon /> Sign In
+            </Link>
+          </Button>
+          <SheetDescription></SheetDescription>
+        </SheetContent>
+      </Sheet>
+    </nav>
+  )
+}
